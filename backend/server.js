bd8a8fda52221191563d22d7230088cd392a0566
Chiara Seim <chiara.seim@sjsu.edu>
2021-10-13 22:42:16 -0700

    resolving merge conflicts

--- backend/server.js
+++ backend/server.js
@@ -18,15 +18,12 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
-<<<<<<< HEAD
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 
-=======
->>>>>>> 36e08eec253dc630ee12879e79d9724d5c1bd04a
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -22,29 +21,6 @@ function App() {
       <Route path="/user" component={CreateUser} />
       </div>
     </Router>
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> 36e08eec253dc630ee12879e79d9724d5c1bd04a
   );
 }
 
